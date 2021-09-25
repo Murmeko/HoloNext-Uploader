ff8688286c5aa4ebc33689260b1f52c9b97ca605
Murmeko <yigiterdinc@gmail.com>
2021-09-25 03:30:23 +0300

    Initial Commit

---
+++ HoloNext UploaderUITests/HoloNext_UploaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HoloNext_UploaderUITestsLaunchTests.swift
+//  HoloNext UploaderUITests
+//
+//  Created by Yiğit Erdinç on 25.09.2021.
+//
+
+import XCTest
+
+class HoloNext_UploaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
